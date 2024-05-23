a87e853a5eb58820e120c083708756c8b90e309f
danielemantovani <daniele.mantovani124@gmail.com>
2024-05-23 19:27:16 +0200

    scaffolding: dischi

--- index.php
+++ index.php
@@ -7,10 +7,29 @@
     <title>Boolfy</title>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <!-- my style -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- Axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- Vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 
 <body>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- js -->
+    <script src="./js/script.js"></script>
 </body>
 
 </html>
