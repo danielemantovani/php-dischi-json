9495e76b0c3570212292c80a399acf8bae32139e
danielemantovani <daniele.mantovani124@gmail.com>
2024-05-25 15:14:42 +0200

    feat: style

--- index.php
+++ index.php
@@ -5,32 +5,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Boolfy</title>
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <!-- my style -->
     <link rel="stylesheet" href="./css/style.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <!-- Axios -->
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <!-- Vue -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 
-<body>
+<body class="ms_bg">
     <div id="app">
-        <div class="container">
-            <div class="row g-2">
-                <div class="col-12 col-md-6 col-lg-4" v-for="music in arrayMusic">
-                    <div class="card" style="width: 18rem;">
-                        <img :src="music.poster" class="card-img-top" :alt="`${music.title} di ${music.author}`">
-                        <div class="card-body">
-                            <h4 class="card-text text-center">{{music.title}}</h4>
-                            <p class="card-text text-center">{{ music.author }}</p>
-                            <h4 class="card-text text-center">{{music.year}}</h4>
+        <header class="mb-5 img-fluid container-fluid">
+            <img class="ms_spotify-logo p-2" src="./img/Spotify-logo.png" alt="Spotify logo">
+        </header>
+        <main>
+            <div class="container">
+                <div class="row g-2">
+                    <div class="col-4" v-for="music in arrayMusic">
+                        <div class="ms_card_container d-flex justify-content-center p-3">
+                            <div class="ms_card text-white" style="width: 18rem;">
+                                <img :src="music.poster" class="card-img-top mb-2 img-fluid" :alt="`${music.title} di ${music.author}`">
+                                <div class="card-body">
+                                    <h6 class="card-text text-center">{{music.title}}</h4>
+                                    <p class="card-text text-center">{{ music.author }}</p>
+                                    <p class="card-text text-center">{{music.year}}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
     </div>
     <!-- js -->
     <script src="./js/script.js"></script>
